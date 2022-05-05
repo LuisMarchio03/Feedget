022541b80e7cf4864141b3f627144ed7c5caf620
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-05-05 14:25:39 -0300

    Aula 01

---
+++ src/components/Widget.tsx
@@ -0,0 +1,28 @@
+import { ChatTeardropDots } from "phosphor-react";
+import { useState } from "react";
+
+export function Widget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen);
+  }
+
+  return (
+    <div className="absolute bottom-5 right-5">
+      {isWidgetOpen && <p>Hello World</p>}
+
+      <button
+        className="bg-violet-500 rounded-full px-3 h-12 text-white flex items-center group"
+        onClick={toggleWidgetVisibility}
+      >
+        <ChatTeardropDots className="w-6 h-6" />
+
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2"></span>
+          Feedback
+        </span>
+      </button>
+    </div>
+  );
+}
