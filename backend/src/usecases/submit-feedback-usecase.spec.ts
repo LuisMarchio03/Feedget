10ba9f2b417058b91f399150a24b46e985129db6
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-05-15 09:44:30 -0300

    Aula 03

--- backend/src/usecases/submit-feedback-usecase.spec.ts
+++ backend/src/usecases/submit-feedback-usecase.spec.ts
@@ -1,5 +1,54 @@
+import { SubmitFeedbackUseCase } from "./submit-feedback-usecase";
+
+const createFeedbackSpy = jest.fn();
+const sendEmailSpy = jest.fn();
+
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: createFeedbackSpy },
+  { sendMail: sendEmailSpy }
+);
+
 describe("submit feedback", () => {
-  it("should be able to submit a feedback", () => {
-    const submitFeedback = new SubmitFeedbackUseCase({});
+  it("should be able to submit a feedback", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "example comment",
+        screenshot: "data:image/png;base64,fdsfsdfdsdfsffdfsfsdfdsdds",
+      })
+    ).resolves.not.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendEmailSpy).toHaveBeenCalled();
+  });
+
+  it("should not be able to submit a feedback without type", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "",
+        comment: "example comment",
+        screenshot: "data:image/png;base64,fdsfsdfdsdfsffdfsfsdfdsdds",
+      })
+    ).rejects.toThrow();
+  });
+
+  it("should not be able to submit a feedback without comment", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "",
+        screenshot: "data:image/png;base64,fdsfsdfdsdfsffdfsfsdfdsdds",
+      })
+    ).rejects.toThrow();
+  });
+
+  it("should not be able to submit a feedback with an invalid screenshot", async () => {
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "example feedback comment",
+        screenshot: "teste.jpg",
+      })
+    ).rejects.toThrow();
   });
 });
